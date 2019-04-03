cf5072b7f5939ff746f5bf0db2dac692ea69914c
giraffeb <rwpakr07@gmail.com>
2019-04-03 15:55:38 +0900

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs  = require('fs');
+
+var app = http.createServer(function(request, response){
+    var url = request.url;
+    if(request.url == '/'){
+        url = '/index.html';
+    }
+
+    if(request.url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return ;
+        
+    }
+
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+});
+
+app.listen(3000);
